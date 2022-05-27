097ee5056aa322aef8b6b9f67a27d63493346496
Rakesh Tripathi <rakeshtripathi408@gmail.com>
2022-05-27 18:55:28 +0530

    Implementing reddit login using nextauth

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,8 +15,11 @@ import {
   SpeakerphoneIcon,
   VideoCameraIcon,
 } from '@heroicons/react/outline'
+import { signIn, useSession } from 'next-auth/react'
 
 function Header() {
+  const { data: session } = useSession()
+
   return (
     <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
@@ -58,17 +61,43 @@ function Header() {
         <MenuIcon className="icon" />
       </div>
 
-      <div className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex ">
-        <div className="relative h-5 w-5 flex-shrink-0">
-          <Image
-            objectFit="contain"
-            src="https://links.papareact.com/23l"
-            layout="fill"
-            alt=""
-          />
+      {session ? (
+        <div
+          onClick={() => signIn()}
+          className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex "
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image
+              objectFit="contain"
+              src="https://links.papareact.com/23l"
+              layout="fill"
+              alt=""
+            />
+          </div>
+
+          <div className="flex-1 text-xs">
+            <p className="truncate">{session.user?.name}</p>
+            <p className="text-gray-400">1 Karma</p>
+          </div>
+
+          <ChevronDownIcon className="h-5 flex-shrink-0 text-gray-400" />
         </div>
-        <p className="text-gray-400">Sign In</p>
-      </div>
+      ) : (
+        <div
+          onClick={() => signIn()}
+          className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex "
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image
+              objectFit="contain"
+              src="https://links.papareact.com/23l"
+              layout="fill"
+              alt=""
+            />
+          </div>
+          <p className="text-gray-400">Sign In</p>
+        </div>
+      )}
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
@@ -8,8 +7,6 @@ const Home: NextPage = () => {
       <Head>
         <title>Reddit Clone</title>
       </Head>
-
-      <Header />
     </div>
   )
 }
