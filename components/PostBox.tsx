dcd75e7cdaa979c75b19158a26a967238a621505
Rakesh Tripathi <rakeshtripathi408@gmail.com>
2022-05-29 15:07:23 +0530

    Implementing Refetching Post Data Functionality

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -7,7 +7,7 @@ import { useForm } from 'react-hook-form'
 import { useMutation } from '@apollo/client'
 import { ADD_POST, ADD_SUBREDDIT } from '../graphql/mutations'
 import client from '../apollo-client'
-import { GET_SUBREDDIT_BY_TOPIC } from '../graphql/queries'
+import { GET_ALL_POSTS, GET_SUBREDDIT_BY_TOPIC } from '../graphql/queries'
 import toast from 'react-hot-toast'
 
 type FormData = {
@@ -19,7 +19,9 @@ type FormData = {
 
 function PostBox() {
   const { data: session } = useSession()
-  const [addPost] = useMutation(ADD_POST)
+  const [addPost] = useMutation(ADD_POST, {
+    refetchQueries: [GET_ALL_POSTS, 'getPostList'],
+  })
   const [addSubreddit] = useMutation(ADD_SUBREDDIT)
 
   const [imageBoxOpen, setImageBoxOpen] = useState<boolean>(false)
